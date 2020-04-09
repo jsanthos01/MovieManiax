25f351f2acfb10299d641a061b45666919db27c0
jsanthos01 <chinu.s2001@gmail.com>
2020-04-09 14:35:03 -0400

    navbar done

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
+import Navbar from './components/Navbar';
 import './App.css';
 
 function App() {
   return (
     <div>
-      
+      <Navbar />
     </div>
   );
 }
