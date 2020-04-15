f80f7d12edc50ea3dc28df3fcc187881b5eb4db0
Akanksha Gupta <akanksha.rapti@gmail.com>
2020-04-15 12:45:31 -0400

    added homepage

---
+++ client/src/components/HomePage/HomePage.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "../style/HomePage.css"
+
+function HomePage(){
+    return(
+        <div class="HomeMain">  
+           <div class="image1"></div>
+           <div class="image2"></div>
+           <div class="image3"></div>
+           <div class="image4"></div>
+           <div class="image5"></div>
+           <div class="imageContainer">
+           </div>
+           <div class="container">
+                <div class="heading">
+                   <strong> Movie Maniax</strong>
+                </div>
+            </div>
+            <div class="container2">   
+                <a class="searchBtn" ><strong>Find a movie</strong></a>
+            </div> 
+
+        </div>
+    )
+}
+export default HomePage;
