db9c7bc4c8e7d197b9686b0685f406f1e3b59cee
Akanksha Gupta <akanksha.rapti@gmail.com>
2020-04-19 10:54:02 -0400

    changed fontsize

--- client/src/components/HomePage/HomePage.js
+++ client/src/components/HomePage/HomePage.js
@@ -6,7 +6,7 @@ function HomePage(){
         <div class="homeDiv">
             <div class="container styleCont">
                 <h1 class="display-7 title">MovieManiax</h1>
-                <p class="words">MOVIES.FUN.POPCORN</p>
+                <p class="words"><strong>MOVIES.FUN.POPCORN</strong></p>
                 <div class="btnContainer">
                     <a class="searchButton" href="/searchMovie" role="button">Find Your Movies</a>
                 </div>
