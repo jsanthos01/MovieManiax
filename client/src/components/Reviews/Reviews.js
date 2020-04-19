a295b39b05edb28d37b428c3dabbdc0bf9646538
jsanthos01 <chinu.s2001@gmail.com>
2020-04-19 15:57:00 -0400

    reviews css

--- client/src/components/Reviews/Reviews.js
+++ client/src/components/Reviews/Reviews.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react'
 import { useParams } from "react-router-dom";
 import picture from './assets/positive.png'
 import Modal from './Modal'
+import './Reviews.css'
 
 function Reviews() {
     const {id, title} = useParams();
@@ -28,40 +29,48 @@ function Reviews() {
     }, [modalDisplay])
 
     return ( 
-        <div className="result container mb-5" style={{backgroundColor:"white"}}>
+        <div className="result container mb-5">
             <div>
-                <h1 style={{color: "black", padding: "20px"}}>{title}</h1>
+                <h1>{title}</h1>
                 <button type="button" class="btn btn-sm btn-outline-primary m-3"  onClick={() => setModalDisplay(true)}>Add a Review</button>
-                {modalDisplay ? <Modal setModalDisplay={setModalDisplay}  movieId={id} movieName={title} /> : ''}
             </div>
-
             <div class="container">
-                <table class="table table-striped">
-                    <thead class="thead-light">
-                        <tr>
-                        <th scope="col">Rating</th>
-                        <th scope="col">Review</th>
-                        <th scope="col">Writer </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {reviews.map((review) => 
-                            <>
-                            <tr>
-                                <td> <img src={picture} />{review.rating}</td>
-                                <td>{review.comment}
-                                    <hr />
-                                    <p>{review.createdAt}</p>
-                                </td>
-                                <td>{review.user.name}
-                                    { userId === review.user.id ? <button type="button" class="btn btn-danger ml-3" onClick={()=> deleteReview(review._id)} >Delete</button> : ''}
-                                </td>
-                            </tr>  
-                            </>
-                        )}
-                    </tbody>
-                </table>
+                {reviews.map(review => 
+                    <div class="content">
+                        <div class="inner_content">
+                            <div class="card styleCard">
+                                <div class="grouped">
+                                    <div class="avatar">
+                                        <a href="/u/Ruuz?language=en-US">
+                                            <img class="avatar lazyload" src="https://images-platform.99static.com/jQu2xohritutSVmnVq7np7rbkxg=/0x0:1920x1920/500x500/top/smart/99designs-contests-attachments/106/106359/attachment_106359975" data-srcset="https://image.tmdb.org/t/p/w64_and_h64_face/xUObnJSvHrFPsIpoDmb1jiQZLq7.jpg 1x, https://image.tmdb.org/t/p/w128_and_h128_face/xUObnJSvHrFPsIpoDmb1jiQZLq7.jpg 2x" alt="Gimly" />
+                                        </a>
+                                    </div>
+                                    <div class="info">
+                                        <div class="rating_wrapper">
+                                            <h3>{review.user.name}</h3>
+                                            <div class="rounded rating"><i class="fas fa-star pr-2" style={{color: "yellow"}}></i>{review.rating}</div>
+                                        </div>
+                                        <h5>Written on {review.createdAt}</h5>
+                                    </div>
+                                </div>
+                                <div class="teaser">
+                                    <p>{review.comment}</p>
+                                </div>
+                                <ul class="nav justify-content-start">
+                                    <li class="nav-item ">
+                                        <i class="comments far fa-comment"></i><span id="number${fetchPosts[i].post_id}">5</span>
+                                        <i class="thumbsUp far fa-thumbs-up"></i><span id="comment${fetchPosts[i].post_id}" >9</span>
+                                        { userId === review.user.id ? <i class=" trash fas fa-trash" onClick={()=> deleteReview(review._id)} ></i> : ''}
+
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                )}
+                  
             </div>
+            {modalDisplay ? <Modal setModalDisplay={setModalDisplay}  movieId={id} movieName={title} /> : ''}
         </div>
     )
 }
