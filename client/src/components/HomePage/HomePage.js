900b5d9789e9e08bdcc8ebb7260f363eb9147498
Sara Munir <saramunir011@gmail.com>
2020-04-20 19:07:14 -0400

    fixed homepage hero image

--- client/src/components/HomePage/HomePage.js
+++ client/src/components/HomePage/HomePage.js
@@ -1,14 +1,17 @@
 import React from "react";
 import "../style/HomePage.css"
-
+ 
 function HomePage(){
     return(
-        <div class="homeDiv ">
-            <div class="container styleCont">
-                <h1 class="display-7 title">MovieManiax</h1>
-                <p class="words"><strong>MOVIES.FUN.POPCORN</strong></p>
-                <div class="btnContainer">
-                    <a class="searchButton" href="/searchMovie" role="button">Find Your Movies</a>
+        <div class="hero2">
+
+            <div class="homeDiv">
+                <div class="container styleCont">
+                    <h1 class="display-7 title">MovieManiax</h1>
+                    <p class="words"><strong>MOVIES.FUN.POPCORN</strong></p>
+                    <div class="btnContainer">
+                        <a class="searchButton" href="/searchMovie" role="button">Find Your Movies</a>
+                    </div>
                 </div>
             </div>
         </div>
